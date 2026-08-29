27552e1299f8bb17f23cea80b45b61407b914f7a
Umer Abdullah <umerabdullah088@gmail.com>
2026-08-29 18:07:09 +0530

    completed the password genrator

---
+++ passwordGenrator/src/App.jsx
@@ -0,0 +1,98 @@
+
+import { useState, useEffect, useRef } from "react"
+function App() {
+  const [sites, setSites] = useState([]);
+  const [password, setPassword] = useState("");
+  const [length, setLength] = useState(8);
+  const [numbers, setNumbers] = useState(false);
+  const [chars, setChars] = useState(false);
+  const inputRef = useRef(null);
+  const passwordGenrator = () => {
+    let pass = "";
+    let str = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+
+    if (numbers) str += "0123456789";
+    if (chars) str += "!~#$%^&*()_+{}[]><.,?"
+
+    for (let i = 0; i < length; i++) {
+      let index = Math.floor(Math.random() * str.length + 1);
+
+      pass += str.charAt(index);
+    }
+
+    setPassword(pass);
+
+  }
+  useEffect(() => {
+    passwordGenrator();
+
+  }, [length, numbers, chars])
+
+  const handleCopy = () => {
+    window.navigator.clipboard.writeText(password)
+    inputRef.current?.select();
+  }
+  return (
+    <div className="min-h-screen bg-gray-100 flex items-center justify-center">
+
+      <div className="bg-white p-8 rounded-xl shadow-lg">
+
+        <input
+          type="text"
+          value={password}
+          readOnly
+          ref={inputRef}
+          className="flex-1 px-4 py-2.5 border border-gray-300 rounded-l-lg
+               bg-gray-50 text-gray-900
+               focus:outline-none focus:ring-2 focus:ring-blue-500"
+
+        />
+
+        <button
+
+          className="px-5 py-2.5 bg-blue-600 text-white font-medium
+               rounded-r-lg hover:bg-blue-700
+               transition-colors"
+          onClick={handleCopy} >
+          Copy
+        </button>
+        <div className="flex items-center gap-4">
+          <label className="font-medium">Length:{length}</label>
+
+          <input
+            type="range"
+            min={5}
+            max={20}
+            className="w-48 cursor-pointer accent-blue-600"
+            onChange={(e) => setLength(e.target.value)}
+          />
+        </div>
+
+        <div className="flex items-center gap-6 mt-4">
+
+          <label className="flex items-center gap-2 cursor-pointer">
+            <input
+              onChange={() => setNumbers((prev) => !prev)}
+              type="checkbox"
+              className="h-4 w-4 accent-blue-600"
+            />
+            <span>Numbers</span>
+          </label>
+
+          <label className="flex items-center gap-2 cursor-pointer">
+            <input
+              onChange={() => setChars((prev) => !prev)}
+              type="checkbox"
+              className="h-4 w-4 accent-blue-600"
+            />
+            <span>Characters</span>
+          </label>
+
+        </div>
+
+      </div>
+    </div>
+  )
+}
+
+export default App
